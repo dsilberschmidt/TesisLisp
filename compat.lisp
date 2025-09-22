e820796d8c1a249233aeaae459979fdb35944b53
Daniel Silberschmidt <daniel.silberschmidt@gmail.com>
2025-09-22 19:34:29 +0200

    shim: robust DE (&OPT fix via CDR), prelude en :tesis (+rest/add1/union/gensym), sombras extra

--- compat.lisp
+++ compat.lisp
@@ -1,4 +1,4 @@
-;;;; compat.lisp  — shim mínimo para dialecto pre-CL
+;;;; compat.lisp — shim mínimo para dialecto pre-CL
 
 ;; --- Lector: [a b c] => (a b c)
 (set-macro-character #\[
@@ -8,14 +8,17 @@
 
 ;; --- Azúcares y utilidades del dialecto
 
+;; DE: mapea &OPT (cualquiera sea el paquete) -> &OPTIONAL
 (defmacro de (name args &body body)
   (labels ((fix (lst)
-             (cond
-               ((null lst) nil)
-               ((and (symbolp (first lst))
-                     (string= (symbol-name (first lst)) "&OPT"))
-                (cons '&optional (fix (rest lst))))
-               (t (cons (first lst) (fix (rest lst)))))))
+             (if (endp lst)
+                 nil
+                 (let ((h (first lst)) (t (cdr lst)))
+                   (cond
+                     ((and (symbolp h)
+                           (string= (symbol-name h) "&OPT"))
+                      (cons '&optional (fix t)))
+                     (t (cons h (fix t))))))))
     `(defun ,name ,(fix args) ,@body)))
 
 (defmacro on  (var) `(setf ,var t))
@@ -42,22 +45,17 @@
 
 ;; ---------------- Tests del shim
 (defun run-shim-tests ()
-  ;; [] lector
   (assert (equal (with-input-from-string (s "[a b (c d)]") (read s))
                  '(a b (c d))))
-  ;; de + &opt
   (de add (x y &opt z) (+ x y (or z 0)))
   (assert (= (add 1 2) 3))
   (assert (= (add 1 2 3) 6))
-  ;; selectq: clave citada
   (let ((r nil))
     (selectq 'foo
       (bar (setf r :bad))
       (foo (setf r :ok)))
     (assert (eq r :ok)))
-  ;; on/off
   (let ((flag nil)) (on flag) (assert flag) (off flag) (assert (null flag)))
-  ;; <> , sub1, append+
   (assert (<> 'a 'b))
   (assert (not (<> 'a 'a)))
   (assert (= (sub1 5) 4))
--- loader.lisp
+++ loader.lisp
@@ -1,6 +1,6 @@
 ;;; loader.lisp
 
-;; 1) Definir paquetes
+;; 1) Paquetes
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (unless (find-package :compat)
     (defpackage :compat
@@ -9,16 +9,28 @@
   (unless (find-package :tesis)
     (defpackage :tesis
       (:use :cl :compat)
-      (:shadow rest last union))))   ; sombras conflictivas
+      ;; Sombras por choques con CL
+      (:shadow rest last union gensym))))
 
-;; 2) Cargar el shim en :compat
+;; 2) Cargar shim en :compat
 (let ((*package* (find-package :compat)))
   (load "compat.lisp"))
 
-;; 3) Trabajar desde el paquete :tesis
+;; 3) Entrar a :tesis
 (in-package :tesis)
 
-;; 4) Compilar primero (detecta issues temprano)
+;; 4) Prelude de compatibilidad en :tesis (antes de compilar .LSP)
+;;    Define las funciones que el código antiguo espera en este paquete.
+(defun rest (lst &optional (n 1)) (nthcdr n lst))
+(defun last (lst) (if (consp lst) (car (cl:last lst)) nil))
+(defun union (a b) (cl:union a b :test #'equal))
+(defun gensym (&optional (prefix "G")) (cl:gensym prefix))
+(defun add1 (x) (1+ x))
+(defun snoc (x lst) (append lst (list x)))
+(defun pertenece (x lst) (member x lst :test #'equal))
+(defun pname (x) (if (symbolp x) (symbol-name x) (error "PNAME: ~S no es símbolo" x)))
+
+;; 5) Compilar primero (detecta issues temprano)
 (mapc #'compile-file
       '("TESIS/UTI.LSP" "TESIS/UNIFICAR.LSP" "TESIS/RENOMBRA.LSP" "TESIS/RESOLVER.LSP"
         "TESIS/TAUTOLOG.LSP" "TESIS/FUSION.LSP" "TESIS/SUBSUME.LSP" "TESIS/MONITOR.LSP"
@@ -26,7 +38,7 @@
         "TESIS/INTERPRE.LSP" "TESIS/UTICLA.LSP" "TESIS/CLAUSAL.LSP" "TESIS/RECICLAR.LSP"
         "TESIS/EVALUAR.LSP" "TESIS/LGC.LSP"))
 
-;; 5) Cargar los FASL
+;; 6) Cargar FASL
 (mapc #'load
       '("TESIS/UTI.fasl" "TESIS/UNIFICAR.fasl" "TESIS/RENOMBRA.fasl" "TESIS/RESOLVER.fasl"
         "TESIS/TAUTOLOG.fasl" "TESIS/FUSION.fasl" "TESIS/SUBSUME.fasl" "TESIS/MONITOR.fasl"
