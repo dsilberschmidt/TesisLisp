7e037d3169327ec38b73fded937dc1db6a6b4cb6
Daniel Silberschmidt <daniel.silberschmidt@gmail.com>
2025-09-22 19:16:37 +0200

    bug en loader

--- loader.lisp
+++ loader.lisp
@@ -1,6 +1,24 @@
-(load "compat.lisp")
+;;; loader.lisp
 
-;; compilar (detecta issues temprano)
+;; 1) Definir paquetes
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (unless (find-package :compat)
+    (defpackage :compat
+      (:use :cl)
+      (:export de on off <> sub1 restn append+ prin2 print2 selectq)))
+  (unless (find-package :tesis)
+    (defpackage :tesis
+      (:use :cl :compat)
+      (:shadow rest last))))   ; << evitar choques con CL:REST y CL:LAST
+
+;; 2) Cargar el shim dentro del paquete :compat (sus símbolos quedan ahí)
+(let ((*package* (find-package :compat)))
+  (load "compat.lisp"))
+
+;; 3) Trabajar desde el paquete :tesis
+(in-package :tesis)
+
+;; 4) Compilar primero (detecta issues temprano)
 (mapc #'compile-file
       '("TESIS/UTI.LSP" "TESIS/UNIFICAR.LSP" "TESIS/RENOMBRA.LSP" "TESIS/RESOLVER.LSP"
         "TESIS/TAUTOLOG.LSP" "TESIS/FUSION.LSP" "TESIS/SUBSUME.LSP" "TESIS/MONITOR.LSP"
@@ -8,7 +26,7 @@
         "TESIS/INTERPRE.LSP" "TESIS/UTICLA.LSP" "TESIS/CLAUSAL.LSP" "TESIS/RECICLAR.LSP"
         "TESIS/EVALUAR.LSP" "TESIS/LGC.LSP"))
 
-;; cargar
+;; 5) Cargar los FASL
 (mapc #'load
       '("TESIS/UTI.fasl" "TESIS/UNIFICAR.fasl" "TESIS/RENOMBRA.fasl" "TESIS/RESOLVER.fasl"
         "TESIS/TAUTOLOG.fasl" "TESIS/FUSION.fasl" "TESIS/SUBSUME.fasl" "TESIS/MONITOR.fasl"
