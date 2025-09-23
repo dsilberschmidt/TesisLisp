70d2739f951550961dd27bd08799238102909307
Daniel Silberschmidt <daniel.silberschmidt@gmail.com>
2025-09-23 08:07:38 +0200

    compat: MacLisp IF (variadic else); STORE→(lambda (x) x); array-funcall macros; STRING concat; VECTOR extend; muffle notes

--- loader.lisp
+++ loader.lisp
@@ -10,7 +10,7 @@
     (defpackage :tesis
       (:use :cl :compat)
       ;; Sombras por choques/semánticas de dialecto
-      (:shadow rest last union gensym nth if list))))
+      (:shadow rest last union gensym nth if list string vector))))
 
 ;; 2) Cargar shim en :compat
 (let ((*package* (find-package :compat)))
@@ -19,19 +19,17 @@
 ;; 3) Entrar a :tesis
 (in-package :tesis)
 
-;; 3.1) Silenciar notas de compilador (ruido tipo "deleting unreachable code")
+;; 3.1) Silenciar notas del compilador (ruido)
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (declaim (sb-ext:muffle-conditions sb-ext:compiler-note)))
 
-;; 4) Definir PRIMERO la macro IF (MacLisp-style):
-;; (if TEST THEN . ELSE-FORMS)  => (cl:if TEST THEN (cl:progn ...ELSE-FORMS...))
+;; 4) Macro IF (MacLisp): (if TEST THEN . ELSE-FORMS)
 (defmacro if (test then &rest else-forms)
-  (cond
-    ((null else-forms) `(cl:if ,test ,then))
-    (t `(cl:if ,test ,then (cl:progn ,@else-forms)))))
+  (if else-forms
+      `(cl:if ,test ,then (cl:progn ,@else-forms))
+      `(cl:if ,test ,then)))
 
 ;; 5) Prelude de compatibilidad (antes de compilar .LSP)
-
 (defun rest (lst &optional (n 1)) (cl:nthcdr n lst))
 (defun last (lst) (cl:if (cl:consp lst) (cl:car (cl:last lst)) nil))
 (defun union (a b) (cl:union a b :test #'cl:equal))
@@ -41,28 +39,67 @@
 (defun snoc (x lst) (cl:append lst (cl:list x)))
 (defun pertenece (x lst) (cl:member x lst :test #'cl:equal))
 
-;; MacLisp-style PNAME: lista de códigos ASCII del nombre del símbolo
+;; PNAME (lista de códigos)
 (defun pname (x)
-  (cl:map 'cl:list #'cl:char-code
+  (cl:map 'cl:string #'cl:char-code
           (etypecase x
             (symbol (cl:symbol-name x))
             (string x))))
 
-;; MacLisp-style NTH: (nth lista n)  (orden invertido respecto a CL)
+;; NTH estilo MacLisp: (nth lista n)
 (defun nth (lst n)
   (etypecase lst
     (list (cl:nth n lst))
     (string (cl:char-code (cl:char lst n)))))
 
 ;; Vars heredadas que aparecen MUY temprano
-(defparameter || nil)                 ;; símbolo de nombre vacío
-(defparameter ?  (cl:char-code #\?))  ;; comparado con = en código legado
+(defparameter || nil)
+(defparameter ?  (cl:char-code #\?))
 
-;; En código histórico: (MAP-2-AR LIST ...) pasa LIST “por nombre”.
-;; Sombramos LIST y lo ligamos a la función CL:LIST para ese uso.
+;; LIST “por nombre” en (MAP-2-AR LIST ...)
 (defparameter list #'cl:list)
 
-;; Macro DF (usada para ON/OFF de banderas en lote)
+;; STRING del dialecto: concatena símbolos/strings/char-codes/listas de codes/etc.
+(defun string (&rest parts)
+  (let ((pieces
+          (mapcar (lambda (p)
+                    (cond
+                      ((cl:stringp p) p)
+                      ((cl:symbolp p) (cl:symbol-name p))
+                      ((cl:characterp p) (cl:string p))
+                      ((cl:integerp p) (cl:string (cl:code-char p)))
+                      ((and (cl:listp p) (every #'cl:integerp p))
+                       (cl:map 'cl:string #'cl:code-char (coerce p 'vector)))
+                      (t (princ-to-string p))))
+                  parts)))
+    (apply #'cl:concatenate 'cl:string pieces)))
+
+;; VECTOR del dialecto: si primer arg es vector, devuelve vector extendido
+(defun vector (&rest args)
+  (if (and args (cl:vectorp (first args)))
+      (let ((vec (first args))
+            (more (rest args)))
+        (cl:concatenate 'vector vec (coerce more 'vector)))
+      (apply #'cl:vector args)))
+
+;; Emulación array-funcall (MacLisp):
+(defvar vector-raices (make-array 0 :element-type t))
+(defvar vector-numeros (make-array 0 :element-type t))
+
+(defmacro vector-raices (i) `(cl:aref vector-raices ,i))
+(defmacro vector-numeros (i) `(cl:aref vector-numeros ,i))
+
+(defun memvec (item vec) (position item vec :test #'cl:equal))
+(defun store (vec i val)
+  (setf (cl:aref vec i) val)
+  ;; para soportar ((STORE ...) POSICION) => POSICION
+  (cl:lambda (x) x))
+
+(defun insert (name)
+  ;; Inserta símbolo (intern) a partir del string NAME en el paquete :TESIS
+  (cl:intern name (find-package :tesis)))
+
+;; Macro DF (ON/OFF de banderas)
 (defmacro df (fn (flags) &body _)
   (declare (ignore _))
   (let ((tmp (cl:gensym "FLAGS")))
